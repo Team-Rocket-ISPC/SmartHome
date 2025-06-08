b877dd3546d1179b4fdbf749c0c6cbba3e34b630
Christian Murua <cristianmurua.18@gmail.com>
2025-06-08 15:33:46 -0300

    Modularizo mas main

--- evidencia_tres/app_consola/main.py
+++ evidencia_tres/app_consola/main.py
@@ -1,9 +1,7 @@
-
-from dispositivos import buscar_dispositivo, eliminar_dispositivo, listar_dispositivos, nuevo_dispositivo
-from menu import menu_automatizaciones, mostrar_opciones_principal_de_app, mostrar_opciones_para_admin, mostrar_opciones_para_estandar 
-from automatizaciones import consultar_automatizaciones_activas, iniciar_automatizacion_aspiradora, iniciar_automatizacion_luces_del_patio
+from menu import llamar_opciones_para_admin, llamar_opciones_para_estandar, mostrar_opciones_principal_de_app, mostrar_opciones_para_admin, mostrar_opciones_para_estandar 
+from automatizaciones import iniciar_automatizacion_aspiradora, iniciar_automatizacion_luces_del_patio
 import datetime
-from auth import verificar_rol, cambiar_rol, consultar_perfil, listar_usuarios, login, logout, register
+from auth import verificar_rol, login, register
 from states import states
 def main():
     print(f"----BIENVENIDO A SMART HOME SOLUTIONS----{datetime.datetime.now().strftime("%H:%M")}")
@@ -24,48 +22,11 @@ def main():
                     match role:
                         case 'admin':
                             mostrar_opciones_para_admin()
-                            opcion=input('Ingrese una opcion: ')
-                            if(opcion == '1'):
-                                listar_dispositivos()
-                            elif(opcion == '2'):
-                                nuevo_dispositivo()
-                            elif(opcion == '3'):
-                                eliminar_dispositivo()
-                            elif(opcion == '4'):
-                                buscar_dispositivo()                                
-                            elif(opcion == '5'):
-                                consultar_automatizaciones_activas()                                
-                            elif(opcion=="6"):
-                                menu_automatizaciones()                                
-                            elif(opcion=="7"):
-                                listar_usuarios()
-                            elif(opcion=="8"):
-                                consultar_perfil()
-                            elif(opcion=="9"):
-                                cambiar_rol()                                
-                            elif(opcion=="10"):
-                                logout()
-                            elif(opcion == '0'):
-                                print("Gracias vuelva pronto")
-                                return
+                            llamar_opciones_para_admin()
                             
                         case 'estandar':
                             mostrar_opciones_para_estandar()
-                            opcion=input('Ingrese una opcion: ')
-                            if(opcion == '1'):
-                               listar_dispositivos()
-                            elif(opcion == '2'):
-                                consultar_perfil()
-                            elif(opcion == '3'):
-                                consultar_automatizaciones_activas()
-                            elif(opcion == '4'):
-                                menu_automatizaciones()
-                            elif(opcion == '5'):
-                                logout()
-                            elif(opcion == '0'):
-                                print("Gracias vuelva pronto")    
-                                return
-                
+                            llamar_opciones_para_estandar()
             case _:
                 print("la opcion ingresada no es correcta")
 
--- evidencia_tres/app_consola/menu.py
+++ evidencia_tres/app_consola/menu.py
@@ -1,5 +1,7 @@
+from auth import cambiar_rol, consultar_perfil, listar_usuarios, logout
 from mensajes_consola import formatear_mensaje_consola
-from automatizaciones import cambiar_estado_automatizacion_aspiradora, iniciar_automatizacion_aspiradora, cambiar_estado_automatizacion_luces_del_patio, iniciar_automatizacion_luces_del_patio
+from dispositivos import buscar_dispositivo, eliminar_dispositivo, listar_dispositivos, nuevo_dispositivo
+from automatizaciones import cambiar_estado_automatizacion_aspiradora, consultar_automatizaciones_activas, iniciar_automatizacion_aspiradora, cambiar_estado_automatizacion_luces_del_patio, iniciar_automatizacion_luces_del_patio
 from states import states
 
 
@@ -43,6 +45,33 @@ def mostrar_opciones_para_admin():
     print("10. Cerrar sesión")
     print("0. Salir")
 
+def llamar_opciones_para_admin():
+    opcion=input('Ingrese una opcion: ')
+    if(opcion == '1'):
+        listar_dispositivos()
+    elif(opcion == '2'):
+        nuevo_dispositivo()
+    elif(opcion == '3'):
+        eliminar_dispositivo()
+    elif(opcion == '4'):
+        buscar_dispositivo()                                
+    elif(opcion == '5'):
+        consultar_automatizaciones_activas()                                
+    elif(opcion=="6"):
+        menu_automatizaciones()                                
+    elif(opcion=="7"):
+        listar_usuarios()
+    elif(opcion=="8"):
+        consultar_perfil()
+    elif(opcion=="9"):
+        cambiar_rol()                                
+    elif(opcion=="10"):
+        logout()
+    elif(opcion == '0'):
+        print("Gracias vuelva pronto")
+        return
+                                
+
 def mostrar_opciones_para_estandar():
     #Permitir consultar los datos personales.
     #Permitir activar/ejecutar/”configurar”(debería estar predefinida) la automatización.
@@ -53,3 +82,19 @@ def mostrar_opciones_para_estandar():
     print("4. Activar/desactivar automatizaciones")
     print("5. Cerrar sesión")
     print("0. Salir")
+
+def llamar_opciones_para_estandar():
+    opcion=input('Ingrese una opcion: ')
+    if(opcion == '1'):
+        listar_dispositivos()
+    elif(opcion == '2'):
+        consultar_perfil()
+    elif(opcion == '3'):
+        consultar_automatizaciones_activas()
+    elif(opcion == '4'):
+        menu_automatizaciones()
+    elif(opcion == '5'):
+        logout()
+    elif(opcion == '0'):
+        print("Gracias vuelva pronto")    
+        return
