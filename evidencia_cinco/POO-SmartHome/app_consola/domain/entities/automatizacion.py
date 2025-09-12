e0da76d36014ed100c1093f25cdfe1bad3547606
MarianoBattista <battista.mariano.i@gmail.com>
2025-09-12 16:09:00 -0300

    Se agrego Word con analisis y justificacion del diagrama de clases

---
+++ evidencia_cinco/POO-SmartHome/app_consola/domain/entities/automatizacion.py
@@ -0,0 +1,13 @@
+class Automatizacion:
+    def __init__(self,nombre, hora_inicio, hora_fin, estado):
+        self.nombre = nombre
+        self.hora_inicio = hora_fin
+        self.hora_fin = hora_fin
+        #self.id_automatizacion = generar de manera procedural
+        #self. id_vivienda = traer de tabla vivienda
+
+    #def crear_automatizacion():
+
+    #def eliminar_automatizacion():
+
+    
