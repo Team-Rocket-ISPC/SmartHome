213bf198aa17cf1cd2e5b0507545985557856cf5
MarianoBattista <battista.mariano.i@gmail.com>
2025-09-13 12:54:00 -0300

    Se escribio el codigo para el modulo automatizacion_objetivo.py

---
+++ evidencia_cinco/POO-SmartHome/app_consola/domain/entities/automatizacion_objetivo.py
@@ -0,0 +1,17 @@
+from dispositivo import Dispositivo
+from ubicacion import Ubicacion #Revisar al finalizar que se hayan nomeclado igual los modulos y clases
+from tipo_dispositivo import TipoDispositivo #Revisar al finalizar que se hayan nomeclado igual los modulos y clases
+from automatizacion import Automatizacion
+
+class AutomatizacionObjetivo:
+    def __init__(self, automatizacion, tipo_dispositivo, ubicacion):
+        self.automatizacion = automatizacion
+        self.tipo_dispositivo = tipo_dispositivo
+        self.ubicacion = ubicacion
+        automatizacion.objetivos.append(self)
+
+    def aplicar_en(self): #Se invoca en automatizacion.py
+        print(f"Aplicando automatización '{self.automatizacion.nombre}' en {self.ubicacion.nombre}")
+        for dispositivo in self.ubicacion.dispositivos:
+            if dispositivo.id_tipo == self.tipo_dispositivo.id_tipo:
+                dispositivo.encender_apagar()
