c963ae6271d42d423e239a8d26b7ede0ec87a682
BATTISTA <SE42293@grupo.ypf.com>
2025-09-26 09:27:51 -0300

    Actualización de scripts en evidencia_tres/app_consola

--- evidencia_tres/app_consola/usuario.py
+++ evidencia_tres/app_consola/usuario.py
@@ -2,42 +2,6 @@ from base_de_datos import base_de_datos
 from auth import * 
 from states import states
 
-class Usuario:
-    def __init__(self, correo: str, contraseña: str, nombre: str):
-        self.correo = correo
-        self.contraseña = contraseña
-        self.nombre = nombre
-    
-    @property
-    def correo(self):
-        return self._correo
-
-    @correo.setter
-    def correo(self, value):
-        if "@" not in value or "." not in value:
-            raise ValueError("Correo inválido")
-        self._correo = value
-
-    @property
-    def nombre(self):
-        return self._nombre
-
-    @nombre.setter
-    def nombre(self, value):
-        if not value or len(value) < 3:
-            raise ValueError("El nombre debe tener al menos 3 caracteres")
-        self._nombre = value
-
-    @property
-    def contraseña(self):
-        return self._contraseña
-
-    @contraseña.setter
-    def contraseña(self, value):
-        if not value or len(value) < 4:
-            raise ValueError("La contraseña debe tener al menos 4 caracteres")
-        self._contraseña = value
-
 def crear_usuario():
     nuevo_usuario = Usuario()
     correo = input("Ingrese correo electronico: ").strip().lower()
