8482298b8f7ac99b3d737041f023ce2c9a2f8874
Christian Murua <cristianmurua.18@gmail.com>
2025-10-12 11:05:38 -0300

    vuelvo atras cambio en conexion

--- evidencia_cinco/POO-SmartHome/app_consola/conn/db_conn.py
+++ evidencia_cinco/POO-SmartHome/app_consola/conn/db_conn.py
@@ -12,8 +12,6 @@ class DBConn:
         self.password = os.getenv("DB_PASSWORD")
         self.database = os.getenv("DB_NAME")
         self.port = int(os.getenv("DB_PORT"))  # valor por defecto 3306
-        self.auth_plugin = os.getenv("AUTH_PLUGIN")  # 'mysql_native_password'
-        self.use_pure = os.getenv("USE_PURE")  # 'True' o 'False'
 
     def connect(self):
         try:
@@ -23,8 +21,6 @@ class DBConn:
                 password=self.password,
                 database=self.database,
                 port=self.port,
-                auth_plugin=self.auth_plugin,
-                use_pure=self.use_pure
             )
             return conn
         except mysql.connector.Error as e:
